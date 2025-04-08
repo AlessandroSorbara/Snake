fd3eb6f070e6742e49d27da3e5f51308cbaf1470
Alessandro Sorbara <alessandrosorbara2004@gmail.com>
2025-04-08 22:08:32 +0200

    updated Snake and Direction class

--- src/com/snakegame/model/Snake.java
+++ src/com/snakegame/model/Snake.java
@@ -1,6 +1,18 @@
 package com.snakegame.model;
 
+import java.util.ArrayList;
+
 public class Snake {
 
+    private ArrayList<Point> snake;
+    private Direction direction;
+    private boolean alive;
 
+    public Snake(Point head) {
+        this.snake = new ArrayList<Point>();
+        this.snake.add(head);
+        this.snake.add(new Point(head.getX() + 1, head.getY() + 1));
+        this.direction = Direction.RIGHT;
+        this.alive = true;
+    }
 }
